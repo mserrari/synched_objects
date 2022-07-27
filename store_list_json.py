8f1a47cab982bf69ca76dde87e9973f65b9e8da2
Mohamed Serrari <43576891+MohamedSerrari@users.noreply.github.com>
2022-07-27 18:33:34 +0200

    improved json driver
    improved json driver:
    - json file termination with `]` is done on the fly not when closing the file which is safer

--- store_list_json.py
+++ store_list_json.py
@@ -6,7 +6,6 @@ import os
 
 INDENT = 4
 SEP = '\n'
-DTYPE = ']'
 
 def assert_type(object: Any, name: str, types: Union[Tuple[Type], Type]):
     
@@ -23,31 +22,13 @@ def assert_type(object: Any, name: str, types: Union[Tuple[Type], Type]):
                         got type {typename(type(object))}")
     
 
-def get_mode(filename: Union[str, Path], overwrite: bool = False, append: bool = False):
-    """
-    Three cases:
-    
-    1. overwrite is True: Create new file with mode 'wb'
-    2. overwrite if False and append is True: Append to existing file mode 'rb+'
-    3. overwrite if False and append is False: Raise an error if there is an existing file, mode 'wb'
-    
-    """
-    
-    mode = 'wb' # overwrite file wether it exists or not
-    if not overwrite and Path(filename).is_file():
-        if not append:
-            raise FileExistsError(f'File already exists {filename}')
-        
-        mode = 'rb+'
-        
-    return mode
 
 class JsonDriver:
     def __init__(self, filename: str, overwrite: bool = False, append: bool = False) -> None:
         
         assert_type(filename, 'filename', (str, Path))
         assert_type(overwrite, 'overwrite', bool)
-        assert_type(append, 'append', [bool])
+        assert_type(append, 'append', bool)
         
         self.filename = Path(filename)
         self.overwrite = overwrite
@@ -58,41 +39,54 @@ class JsonDriver:
     def __post_init__(self):
         """don't include start only when appending non empty file"""
         
-        self.mode = get_mode(self.filename, overwrite=self.overwrite, append=self.append)
-        self.file = open(self.filename, mode=self.mode)
+        # File exists and we are not overwriting nor appending
+        if Path(self.filename).is_file() and not (self.overwrite or self.append):
+            raise FileExistsError(f'File already exists {self.filename}')
         
-        self.keep_left = True
-        self.offset_cursor = False
-        if self.mode == 'rb+':
-            self.file.seek(0, os.SEEK_END) # go to end of file
-            if self.file.tell() != 0: # if current position is truish (i.e != 0) so file is not empty       
-                self.keep_left = False
-                self.offset_cursor = True
+        self.isempty = True
+        
+        # Create new file if file does not exist or we overwriting
+        if not Path(self.filename).is_file() or self.overwrite:
+            open(self.filename, "w").close()
+        
+        # Open the file in read write update binary mode
+        self.file = open(self.filename, mode='rb+')
+        assert self.file is not None, f'Could not create file {self.filename}'
+        
+        if self.append and not self.overwrite:
+            # Check if file is empty
+            
+            self.file.seek(0, os.SEEK_END)  # Go to end of file
+            if self.file.tell() != 0:
+                # if current position is truish (i.e != 0) then file is not empty       
+                self.isempty = False
             else:
+                # reset the cursor when file is empty
                 self.file.seek(0)
-        
-        
+        else:
+            self.file.truncate()
+
     def write(self, data: List[dict]):
+        
+        if len(data) == 0: return
 
         output = json.dumps(data, indent=INDENT)
         
-        END = -len(SEP)-len(']')
-        if self.keep_left:
-            self.keep_left = False
-            output = output[:END] # remove last closed bracket `\n]`
-        else:
-            START = len(SEP)+INDENT+len(']')
-            output = f",{SEP}{' '*INDENT}{output[START:END]}"
-        
-        # position the cursor for proper appending
-        if self.offset_cursor:
-            self.offset_cursor = False
+        # When file is empty we write the ouput directly
+        # Else we have to trim the brackets for proper appending
+        if not self.isempty:
+            # Roll back the cursor to overwrite the last `\n]`
+            END = -len(SEP)-len(']')
             self.file.seek(END, os.SEEK_END)
+            
+            # Format string to be appended
+            START = len('[')+len(SEP)+INDENT
+            output = f",{SEP}{' '*INDENT}" + output[START:]
         
         self.file.write(output.encode())
+        self.isempty = False
     
     def __del__(self):
-        self.file.write(f'{SEP}]'.encode())
         self.file.close()
 
 class StoreListJson():
@@ -175,10 +169,10 @@ def test_json_driver():
     del drv
     
     
-    saved_data = json.load(open(filename, mode='r'))
-    print(saved_data == data)
-    print(saved_data)
-    print(data)
+    # saved_data = json.load(open(filename, mode='r'))
+    # print(saved_data == data)
+    # print(saved_data)
+    # print(data)
     
     
 def test_rb_ab():
@@ -186,8 +180,16 @@ def test_rb_ab():
     file.write('ABCDEFGHIJKLMONQRSTUVWXYZ'.encode())
     file.close()
     
+    
+    
+    
+    
     file = open('tt.json', mode='rb+')
-    file.seek(-10, os.SEEK_END)
+    file.seek(-20, os.SEEK_END)
+    
+    file.truncate()
+    
+    
     file.write('123456789'.encode())
     file.close()
     
@@ -200,4 +202,4 @@ def test_storelist_json():
 if __name__ == '__main__':
     
     test_json_driver()
-
+    # test_rb_ab()
