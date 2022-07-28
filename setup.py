cb5d701d68b16f5f2e67d51be61a8c00603f95ef
Mohamed Serrari <43576891+MohamedSerrari@users.noreply.github.com>
2022-07-28 14:41:49 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='synched_objects',
+      version='0.1',
+      description='Auto synching python objects',
+      url='https://github.com/MohamedSerrari/synched_objects',
+      author='MohamedSerrari',
+      author_email='serrari.med@hotmail.com',
+      license='MIT',
+      packages=['synched_objects'],
+      zip_safe=False)
